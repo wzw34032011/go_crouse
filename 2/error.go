51415988556126c8c3850da2e754056b9864286e
wzw <wzw34032011@126.com>
2021-04-22 22:20:17 +0800

    update error

--- 2/error.go
+++ 2/error.go
@@ -2,30 +2,61 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"github.com/pkg/errors"
 	"log"
 )
 
-type MyError struct {
+// 我们在数据库操作的时候，比如 dao 层中当遇到一个 sql.ErrNoRows 的时候，是否应该 Wrap 这个 error，抛给上层。为什么，应该怎么做请写出代码？
+
+// sql.ErrNoRows 是 Scan(dest ...interface{}) 方法在无查询记录时返回的错误，需要抛给上层。
+// 第一种情况，能查询到记录，但是这条记录的字段值是空值，这时Scan方法给dest字段赋值为空值
+// 第二种情况，没有查询到记录，Scan方法不会给dest字段赋值，dest变量为初始零值（空值）
+// 情况一和二都返回了一样的空值，如果不上抛错误，上层无法得知数据是否存在
+
+type DaoError struct {
 	msg      string
 	emptyRow bool
 	err      error
 }
 
-func (de *MyError) Cause() error {
+func (de *DaoError) Cause() error {
 	return de.err
 }
 
-func (de *MyError) Unwrap() error {
+func (de *DaoError) Unwrap() error {
 	return de.err
 }
 
-func (de *MyError) Error() string {
+func (de *DaoError) Error() string {
 	return de.msg
 }
 
-func (de *MyError) IsEmptyRow() bool {
+func (de *DaoError) IsEmptyRow() bool {
 	return de.emptyRow
+}
+
+type ServiceError struct {
+	code int
+	msg  string
+	err  error
+}
+
+func (se *ServiceError) Cause() error {
+	return se.err
+}
+
+func (se *ServiceError) Unwrap() error {
+	return se.err
+}
+
+func (se *ServiceError) Error() string {
+	return fmt.Sprintf("code:%d,msg:%s", se.code, se.msg)
+}
+
+func (se *ServiceError) Is(tag error) bool {
+	_, ok := tag.(*ServiceError)
+	return ok
 }
 
 func WrapStackOnce(err error, message string) error {
@@ -40,60 +71,70 @@ func WrapStackOnce(err error, message string) error {
 	}
 }
 
-func Dao() error {
+func Dao() (string, error) {
+	var data string
 	var err = sql.ErrNoRows
 	//var err = sql.ErrConnDone
 
-	var me *MyError
-	var we error
-	switch {
-	case errors.Is(err, sql.ErrNoRows):
-		me = &MyError{msg: "query not find", emptyRow: true, err: err}
-		we = WrapStackOnce(me, "dao we")
-	case err != nil:
-		me = &MyError{msg: "query err", emptyRow: false, err: err}
-		we = WrapStackOnce(me, "dao we")
-	default:
-		we = nil
+	var de *DaoError
+	var we error = nil
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			de = &DaoError{msg: "query not find", emptyRow: true, err: err}
+			we = WrapStackOnce(de, "dao error")
+		} else {
+			de = &DaoError{msg: "connect err", emptyRow: false, err: err}
+			we = WrapStackOnce(de, "dao error")
+		}
+	} else {
+		data = "正常数据"
 	}
-	return we
+	return data, we
 }
 
-func Service() error {
-	err := Dao()
+func Service() (string, error) {
+	data, err := Dao()
 	if err != nil {
-		var me *MyError
-		if errors.As(err, &me) && me.IsEmptyRow() {
+		var de *DaoError
+		if errors.As(err, &de) && de.IsEmptyRow() {
 			//空数据错误
-			if canHandle := false; canHandle {
-				//处理错误代码,降级业务流程
-				return nil
+			if returnError := true; returnError {
+				//错误上抛
+				se := &ServiceError{
+					code: 1,
+					msg:  "未查询到数据",
+					err:  err,
+				}
+				return data, WrapStackOnce(se, "service error")
 			} else {
-				//无法处理错误，返回给调用者
-				return WrapStackOnce(err, "未查询到数据")
+				//降级处理
+				return "兜底数据", nil
 			}
 		} else {
-			//其他错误
-			if canHandle := false; canHandle {
-				//处理错误代码,降级业务流程
-				return nil
-			} else {
-				//无法处理错误，返回给调用者
-				return WrapStackOnce(err, "Dao 异常")
+			//无法处理错误，错误上抛
+			return data, WrapStackOnce(err, "service error")
+		}
+	}
+
+	//正常流程
+	return data, nil
+}
+
+func main() {
+	data, err := Service()
+	if err != nil {
+		if !errors.Is(err, new(ServiceError)) {
+			//错误无法处理，写log
+			log.Printf("stack trace:\n%+v\n", err)
+			log.Printf("original error: %v", errors.Cause(err))
+
+			err = &ServiceError{
+				code: 2,
+				msg:  "unknown error",
+				err:  err,
 			}
 		}
 	}
 
-	//正常业务流程
-	return WrapStackOnce(err, "其他业务错误")
-}
-
-func main() {
-	err := Service()
-	if err != nil {
-		log.Printf("stack trace:\n%+v\n", err)
-		log.Printf("original error: %v", errors.Cause(err))
-	} else {
-		log.Println("success")
-	}
+	fmt.Printf("data=%s\nerr=%v", data, err)
 }
